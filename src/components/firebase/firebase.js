08bdb15a77189448d11e21644f4f957cc93a02c3
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2026-02-10 00:04:08 +0530

    10-2-26

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 //import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -16,4 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 //const analytics = getAnalytics(app);
+
+export { app, auth };
