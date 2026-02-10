2b8d3cee1831121c84655b80501a2bab20f41f98
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2026-02-10 23:10:47 +0530

    10-02

--- src/components/contexts/authContext/index.js
+++ src/components/contexts/authContext/index.js
@@ -1,7 +1,42 @@
-import { useState, useEffect } from "react";
+import { auth } from "./../../firebase/firebase";
+import { useState, useEffect, useContext } from "react";
 
 const AuthContext = React.createContext();
 
-export function AuthProvider({ childern }) {
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(auth, initializeUser);
+    return unsubscribe;
+  }, []);
+
+  async function initializeUser(user) {
+    if (user) {
+      setCurrentUser({ ...user });
+      setUserLoggedIn(true);
+    } else {
+      setCurrentUser(null);
+      setUserLoggedIn(false);
+    }
+    setLoading(false);
+  }
+
+  const value = {
+    currentUser,
+    userLoggedIn,
+    loading,
+  };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
 }
