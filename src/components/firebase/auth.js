d1f3f2551b1bcb34eaf63c472c38e9d1da7df141
geekaryan <aryan.rana2021@vitbhopal.ac.in>
2026-02-17 23:16:04 +0530

    17-2

--- src/components/firebase/auth.js
+++ src/components/firebase/auth.js
@@ -1,6 +1,28 @@
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+} from "firebase/auth";
 import { auth } from "./firebase";
 
+//sign in with email and password
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithEmailAndPassword = async (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
+//sign in with google
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await auth.signInWithPopup(auth, provider);
+  return result;
+};
+
+//singOut User from their acount
+
+export const doSignOut = () => {
+  return auth.signOut();
 };
